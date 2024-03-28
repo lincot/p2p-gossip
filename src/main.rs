34327b2cc87c034aac6bfb235ba3713617acc139
lincot <lincot@disroot.org>
2024-03-28 17:57:03 +0300

    handle disconnection

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,6 @@
 #![feature(lazy_cell)]
 
 // TODO: add comments
-// TODO: handle disconnections
 
 mod config;
 mod log;
@@ -55,11 +54,10 @@ async fn main() -> io::Result<()> {
     let (tx, _rx) = broadcast::channel::<String>(16);
 
     let peers = if let Some(connect) = args.connect {
-        newcomer_connect(&endpoint, connect, &tx).await?
+        initial_connect(&endpoint, connect, &tx).await?
     } else {
-        HashSet::new()
+        Arc::new(Mutex::new(HashSet::new()))
     };
-    let peers = Arc::new(Mutex::new(peers));
 
     if let Some(period) = args.period {
         tokio::spawn(message_producing_loop(
@@ -74,6 +72,11 @@ async fn main() -> io::Result<()> {
     while let Some(connection) = endpoint.accept().await {
         let connection = connection.await?;
 
+        if peers.lock().await.contains(&connection.remote_address()) {
+            connection.close(1u8.into(), b"already connected");
+            continue;
+        }
+
         log(&[
             b"Accepted a connection from ",
             connection.remote_address().to_string().as_bytes(),
@@ -85,24 +88,27 @@ async fn main() -> io::Result<()> {
         send.finish().await?;
 
         peers.lock().await.insert(connection.remote_address());
-        tokio::spawn(handle_connection(connection, tx.subscribe()));
+        tokio::spawn(handle_connection(connection, tx.subscribe(), peers.clone()));
     }
 
     Ok(())
 }
 
-async fn newcomer_connect(
+async fn initial_connect(
     endpoint: &Endpoint,
     connect: SocketAddr,
     tx: &Sender<String>,
-) -> io::Result<HashSet<SocketAddr>> {
+) -> io::Result<Arc<Mutex<HashSet<SocketAddr>>>> {
     // TODO: DNS lookup
     let connection = endpoint.connect(connect, "localhost").unwrap().await?;
     let mut recv = connection.accept_uni().await?;
     let line = recv.read_to_end(1024).await.unwrap();
-    let mut peers: HashSet<SocketAddr> = bincode::deserialize(&line).unwrap();
+    let peers: HashSet<SocketAddr> = bincode::deserialize(&line).unwrap();
+    let peers = Arc::new(Mutex::new(peers));
     let mut new_peers = HashSet::new();
-    for addr in &peers {
+
+    let mut peers_lock = peers.lock().await;
+    for addr in &*peers_lock {
         let connection = endpoint.connect(*addr, "localhost").unwrap().await?;
 
         let mut recv = connection.accept_uni().await?;
@@ -110,11 +116,12 @@ async fn newcomer_connect(
         let new_peers_chunk: Vec<SocketAddr> = bincode::deserialize(&line).unwrap();
         new_peers.extend(new_peers_chunk);
 
-        tokio::spawn(handle_connection(connection, tx.subscribe()));
+        tokio::spawn(handle_connection(connection, tx.subscribe(), peers.clone()));
     }
-    peers.extend(new_peers);
-    peers.insert(connect);
-    tokio::spawn(handle_connection(connection, tx.subscribe()));
+    peers_lock.extend(new_peers);
+    peers_lock.insert(connect);
+    drop(peers_lock);
+    tokio::spawn(handle_connection(connection, tx.subscribe(), peers.clone()));
     Ok(peers)
 }
 
@@ -145,19 +152,52 @@ async fn message_producing_loop(
 
         if !to.is_empty() {
             let msg = generate_random_message(&mut rng);
-            log(&[b"Sending message [", msg.as_bytes(), b" to [", &to, b"]"])?;
+            log(&[b"Sending message [", msg.as_bytes(), b"] to [", &to, b"]"])?;
             tx.send(msg).unwrap();
         }
         tokio::time::sleep_until(end).await;
     }
 }
 
-async fn handle_connection(connection: Connection, rx: Receiver<String>) -> io::Result<()> {
-    tokio::spawn(receiving_loop(connection.clone()));
-    sending_loop(rx, connection).await
+async fn handle_connection(
+    connection: Connection,
+    rx: Receiver<String>,
+    peers: Arc<Mutex<HashSet<SocketAddr>>>,
+) -> io::Result<()> {
+    let res = handle_connection_inner(&connection, rx).await;
+    peers.lock().await.remove(&connection.remote_address());
+    res
 }
 
-async fn receiving_loop(connection: Connection) -> io::Result<()> {
+async fn handle_connection_inner(
+    connection: &Connection,
+    mut rx: Receiver<String>,
+) -> io::Result<()> {
+    tokio::spawn({
+        let connection = connection.clone();
+        async move { sending_loop(&mut rx, &connection).await }
+    });
+    loop {
+        let receiving_res = receiving_loop(connection).await;
+        if let Some(reason) = connection.close_reason() {
+            log(&[
+                b"Closed connection to ",
+                connection.remote_address().to_string().as_bytes(),
+                b", reason: ",
+                reason.to_string().as_bytes(),
+            ])?;
+            return Ok(());
+        }
+        log(&[
+            b"Failed to receive from ",
+            connection.remote_address().to_string().as_bytes(),
+            b", error:",
+            format!("{receiving_res:?}").as_bytes(),
+        ])?;
+    }
+}
+
+async fn receiving_loop(connection: &Connection) -> io::Result<()> {
     let peer_addr = connection.remote_address().to_string();
     loop {
         let mut recv = connection.accept_uni().await?;
@@ -171,7 +211,7 @@ async fn receiving_loop(connection: Connection) -> io::Result<()> {
     }
 }
 
-async fn sending_loop(mut rx: Receiver<String>, connection: Connection) -> Result<(), io::Error> {
+async fn sending_loop(rx: &mut Receiver<String>, connection: &Connection) -> Result<(), io::Error> {
     while let Ok(msg) = rx.recv().await {
         let mut send = connection.open_uni().await?;
         send.write_all(msg.as_bytes()).await?;
